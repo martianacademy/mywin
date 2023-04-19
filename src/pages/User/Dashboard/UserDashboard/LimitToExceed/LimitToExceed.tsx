bf94d57057b4dc8441693cf9a007a368a59e76a1
Suru <martianacademy@gmail.com>
2023-04-20 00:20:30 +0530

    dashboard limit set

--- src/pages/User/Dashboard/UserDashboard/LimitToExceed/LimitToExceed.tsx
+++ src/pages/User/Dashboard/UserDashboard/LimitToExceed/LimitToExceed.tsx
@@ -3,6 +3,7 @@ import {
   CircularProgressLabel,
   Heading,
   HStack,
+  Tag,
   Text,
   VStack,
 } from "@chakra-ui/react";
@@ -26,38 +27,32 @@ export const LimitToExceed = ({
     (idAccountMap?.limitBalanceUSD / idAccountMap?.maxLimitAmount) * 100;
   return (
     <CardContainer>
-      <Heading size="sm">Remaining Limit</Heading>
-      <MotionCircularProgress
+      <Heading size="sm">Limit Reached</Heading>
+      <CircularProgress
         size={200}
         thickness="15px"
+        color="orange.500"
         value={limitPercentage > 0 ? limitPercentage : 0}
-        color="pink.500"
       >
         <CircularProgressLabel>
           {limitPercentage > 0 ? limitPercentage : 0}%
         </CircularProgressLabel>
-      </MotionCircularProgress>
+      </CircularProgress>
       <VStack w="full">
-        <Heading size="md" color="orange.500">
-          ROI Limit
-        </Heading>
-        <Heading size="sm" fontStyle="oblique">
+        <Tag colorScheme="green">ROI Limit</Tag>
+        <Heading size="sm" fontStyle="oblique" fontWeight="semibold">
           {idAccountMap?.maxLimitAmount} {currentNetwork?.MYUSD?.Symbol}
         </Heading>
       </VStack>
       <VStack w="full">
-        <Heading size="md" color="orange.500">
-          Referral Limit
-        </Heading>
-        <Heading size="sm" fontStyle="oblique">
+        <Tag colorScheme="green">Referral Limit</Tag>
+        <Heading size="sm" fontStyle="oblique" fontWeight="semibold">
           {idAccountMap?.maxLimitAmount} {currentNetwork?.MYUSD?.Symbol}
         </Heading>
       </VStack>
       <VStack w="full">
-        <Heading size="md" color="red">
-          Limit Reached
-        </Heading>
-        <Heading size="sm" fontStyle="oblique">
+        <Tag colorScheme="red">Limit Reached</Tag>
+        <Heading size="sm" fontStyle="oblique" fontWeight="semibold">
           {idAccountMap?.limitBalanceUSD} {currentNetwork?.MYUSD?.Symbol}
         </Heading>
       </VStack>
