37cdfdca13d26f9c5530229beba367272e66bea3
Suru <martianacademy@gmail.com>
2023-05-05 09:58:40 +0530

    system under maintanace

--- src/pages/JoinPage/JoinPage.tsx
+++ src/pages/JoinPage/JoinPage.tsx
@@ -1,5 +1,6 @@
-import { VStack } from "@chakra-ui/react";
+import { Heading, VStack } from "@chakra-ui/react";
 import { useEthers } from "@usedapp/core";
+import { Counter } from "../../components/Counter";
 import { JoinUI } from "../../components/JoinUI/JoinUI";
 import { useCoinPrice } from "../../hooks/PriceOracleHooks";
 import { useMinContributionETH } from "../../hooks/ReferralHooks";
@@ -10,7 +11,9 @@ export const JoinPage = () => {
   const coinPrice = useCoinPrice();
   return (
     <VStack w="full" flex={1} py={50}>
-      <JoinUI account={account} chainId={chainId} minContributionETH={minContributionETH} coinPrice={coinPrice}></JoinUI>
+      {/* <JoinUI account={account} chainId={chainId} minContributionETH={minContributionETH} coinPrice={coinPrice}></JoinUI> */}
+      <Heading>System is under maintainance</Heading>
+      <Counter timeinseconds={1683309600}></Counter>
     </VStack>
   );
 };
