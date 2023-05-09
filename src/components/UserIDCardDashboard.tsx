c77933e45c8e7ff11ec297094394fbfc23ce5a86
Suru <martianacademy@gmail.com>
2023-05-09 23:00:22 +0530

    icons added to admin function userIDCardDashboard

--- src/components/UserIDCardDashboard.tsx
+++ src/components/UserIDCardDashboard.tsx
@@ -1,4 +1,6 @@
 import {
+  Avatar,
+  AvatarBadge,
   Badge,
   CircularProgress,
   CircularProgressLabel,
@@ -11,9 +13,20 @@ import {
   useColorModeValue,
   VStack,
   Wrap,
+  Icon,
+  Tooltip,
 } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import React from 'react';
+import {
+  MdAirplanemodeActive,
+  MdAirplanemodeInactive,
+  MdFlashOff,
+  MdFlashOn,
+  MdVisibility,
+  MdVisibilityOff,
+} from 'react-icons/md';
+import { TbWallet, TbWalletOff } from 'react-icons/tb';
 import { AddressZero } from '../constants';
 import {
   useGetIDRewardPaid,
@@ -31,7 +44,15 @@ const backgrounds = [
   `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ECC94B'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%239F7AEA'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%234299E1'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%2348BB78'/%3E%3C/svg%3E")`,
 ];
 
-export const UserIDCardDashboard = ({ id, isAdmin, userAddress }: { id: string, isAdmin: boolean, userAddress?: string }) => {
+export const UserIDCardDashboard = ({
+  id,
+  isAdmin,
+  userAddress,
+}: {
+  id: string;
+  isAdmin: boolean;
+  userAddress?: string;
+}) => {
   const getIDTeam = useGetIDTeam(id);
   const getIDReward = useGetIDRewardPaid(id);
   const idAccountMap = useIDAccount(id);
@@ -74,22 +95,22 @@ export const UserIDCardDashboard = ({ id, isAdmin, userAddress }: { id: string, 
         spacing={5}
         bgColor={useColorModeValue('white', 'gray.900')}
         maxW={300}
-        whileHover={{
-          y: -10,
-          scale: 1.05,
-        }}
-        whileTap={{
-          scale: 0.97,
-        }}
-        transition={{
-          type: 'spring',
-          stiffness: 700,
-        }}
+        // whileHover={{
+        //   y: -10,
+        //   scale: 1.05,
+        // }}
+        // whileTap={{
+        //   scale: 0.97,
+        // }}
+        // transition={{
+        //   type: 'spring',
+        //   stiffness: 700,
+        // }}
       >
         <HStack w="full" justify="space-around">
           <VStack>
             <VStack>
-            <Badge colorScheme="pink" borderRadius="xl">
+              <Badge colorScheme="pink" borderRadius="xl">
                 User ID
               </Badge>
               <Heading size="md">{id}</Heading>
@@ -123,31 +144,98 @@ export const UserIDCardDashboard = ({ id, isAdmin, userAddress }: { id: string, 
             )}
           </CircularProgress>
         </HStack>
-        {isAdmin && <Tag w="200px" p={3} borderRadius="3xl">
-          <VStack w="full">
-            <HStack w="full">
-              <Text fontSize="xs" color={userAccount?.isActive ? "green.400" : "red.400"}>Visibility</Text>
-              <Spacer></Spacer>
-              <Text color={userAccount?.isActive ? "green.400" : "red.400"}>{userAccount?.isActive ? "true" : "false"}</Text>
+        {isAdmin && (
+          <Tag w="200px" borderRadius="3xl">
+            <HStack>
+              <Tooltip
+                label={
+                  userAccount?.isActive
+                    ? 'This ID is visible to its owner.'
+                    : 'This ID is not visible to its owner.'
+                }
+                borderRadius="xl"
+              >
+                <Avatar
+                  bgColor="transparent"
+                  size="sm"
+                  icon={
+                    <Icon
+                      as={
+                        userAccount?.isActive ? MdVisibility : MdVisibilityOff
+                      }
+                      boxSize={7}
+                      color={userAccount?.isActive ? 'green.500' : 'red.500'}
+                    ></Icon>
+                  }
+                ></Avatar>
+              </Tooltip>
+              <Tooltip
+                label={
+                  userAccount?.isActive
+                    ? 'This ID is Active.'
+                    : 'This ID is Incative.'
+                }
+                borderRadius="xl"
+              >
+                <Avatar
+                  bgColor="transparent"
+                  icon={
+                    <Icon
+                      as={
+                        idAccountMap?.isActive
+                          ? MdAirplanemodeActive
+                          : MdAirplanemodeInactive
+                      }
+                      boxSize={7}
+                      color={idAccountMap?.isActive ? 'green.500' : 'red.500'}
+                    ></Icon>
+                  }
+                ></Avatar>
+              </Tooltip>
+              <Tooltip
+                label={
+                  idAccountMap?.canWindraw
+                    ? 'User can withdraw balance from this ID.'
+                    : 'Withdrawal is disabled. User cannot withdraw balance from this ID'
+                }
+                borderRadius="xl"
+              >
+                <Avatar
+                  bgColor="transparent"
+                  size="sm"
+                  icon={
+                    <Icon
+                      as={idAccountMap?.canWindraw ? TbWallet : TbWalletOff}
+                      boxSize={7}
+                      color={idAccountMap?.canWindraw ? 'green.500' : 'red.500'}
+                    ></Icon>
+                  }
+                ></Avatar>
+              </Tooltip>
+              <Tooltip
+                label={
+                  idAccountMap?.isROIDisabled
+                    ? 'User cant see and claim roi. But ROI is still running.'
+                    : 'User can see and claim roi.'
+                }
+                borderRadius="xl"
+              >
+                <Avatar
+                  bgColor="transparent"
+                  size="sm"
+                  icon={
+                    <Icon
+                      as={!idAccountMap?.isROIDisabled ? MdFlashOn : MdFlashOff}
+                      boxSize={7}
+                      color={!idAccountMap?.isROIDisabled ? 'green.500' : 'red.500'}
+                    ></Icon>
+                  }
+                ></Avatar>
+              </Tooltip>
             </HStack>
-            <HStack w="full">
-              <Text fontSize="xs" color={idAccountMap?.isActive ? "green.400" : "red.400"}>Active</Text>
-              <Spacer></Spacer>
-              <Text color={idAccountMap?.isActive ? "green.400" : "red.400"}>{idAccountMap?.isActive ? "true" : "false"}</Text>
-            </HStack>
-            <HStack w="full">
-              <Text fontSize="xs" color={idAccountMap?.canWindraw ? "green.400" : "red.400"}>Withdraw</Text>
-              <Spacer></Spacer>
-              <Text color={idAccountMap?.canWindraw ? "green.400" : "red.400"}>{idAccountMap?.canWindraw ? "true" : "false"}</Text>
-            </HStack>
-            <HStack w="full">
-              <Text fontSize="xs" color={idAccountMap?.isROIDisabled ? "green.400" : "red.400"}>isROIDisabled</Text>
-              <Spacer></Spacer>
-              <Text color={idAccountMap?.isROIDisabled ? "green.400" : "red.400"}>{idAccountMap?.canWindraw ? "true" : "false"}</Text>
-            </HStack>
-          </VStack>
-        </Tag>}
-        
+          </Tag>
+        )}
+
         <Divider />
         {idAccountMap.oldId && (
           <HStack>
@@ -159,12 +247,22 @@ export const UserIDCardDashboard = ({ id, isAdmin, userAddress }: { id: string, 
         )}
 
         <Wrap justify="center" align="center" maxW={300}>
-          <Tag colorScheme="blue">Value: ${Number(idAccountMap.selfBusiness + idAccountMap?.selfBusinessOld)?.toFixed(2)}</Tag>
+          <Tag colorScheme="blue">
+            Value: $
+            {Number(
+              idAccountMap.selfBusiness + idAccountMap?.selfBusinessOld
+            )?.toFixed(2)}
+          </Tag>
           <Tag colorScheme="blue">
             Reward: ${getIDReward.totalRewardPaid.toFixed(2)}
           </Tag>
           <Tag colorScheme="blue">Team: {getIDTeam.teamCount}</Tag>
-          <Tag colorScheme="blue">Business: ${(idAccountMap.teamBusiness + idAccountMap?.teamBusinessOld)?.toFixed(2)}</Tag>
+          <Tag colorScheme="blue">
+            Business: $
+            {(
+              idAccountMap.teamBusiness + idAccountMap?.teamBusinessOld
+            )?.toFixed(2)}
+          </Tag>
         </Wrap>
       </MotionVStack>
     </VStack>
